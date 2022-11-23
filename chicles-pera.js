5720f39db92d23d8df30795f208a0785147d7816
TLLRSK <112385550+TLLRSK@users.noreply.github.com>
2022-11-23 13:16:02 +0100

    refactorizado y solución JS

--- chicles-pera.js
+++ chicles-pera.js
@@ -1,9 +1,9 @@
 function showDeliveryCost(deliveryCost){
-	document.getElementById('delivery-cost').innerText = deliveryCost;
+	document.getElementById('js-delivery-cost').innerText = deliveryCost;
 }
 
 function showTotal(totalPrice){
-	document.getElementById('total-price').innerText = totalPrice;
+	document.getElementById('js-total-price').innerText = totalPrice;
 }
 
 function showBadZipFormat() {
@@ -12,14 +12,33 @@ function showBadZipFormat() {
 
 window.addEventListener('load', () => {
 	const priceWithoutDeliveryCost = 5;
-	const calculateDeliveryButton = document.getElementById('calculate-delivery');
+	const calculateDeliveryButton = document.getElementById('js-calculate-delivery');
 
 	calculateDeliveryButton.addEventListener('click', () => {
-		const zipCode = document.getElementById('zip-code').value;
+		const zipCode = document.getElementById('js-zip-code').value;
 		console.log(zipCode);
 
-		// ADD LOGIC HERE	
+		//CHECK FORMAT
+		if (zipCode.length != 5 || isNaN(zipCode%zipCode)) {
+			return showBadZipFormat();
+		}
+	
+		// ADD DELIVERY COST
+		let deliveryCost = 0;
+		let coin = '€'
 
-
+		if (zipCode[0] == 1 && zipCode[1] == 0) {
+			deliveryCost = 0;
+		} else if (zipCode[0] >= 1 && zipCode[0] <= 5) {
+			deliveryCost = 2;
+		} else if (zipCode[0] >= 6 && zipCode[0] <= 8) {
+			deliveryCost = 3;
+		} else if (zipCode[0] == 9) {
+			deliveryCost = 5;
+		}
+		showDeliveryCost(deliveryCost + coin)
+		
+		//RETURN TOTAL COST
+		return showTotal(priceWithoutDeliveryCost + deliveryCost + coin)
 	})
 });
